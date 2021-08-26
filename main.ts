df0f5811ff36199050604a04447b0a480b144311
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-08-26 16:50:54 -0500

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -503,7 +503,12 @@ function createImages () {
     assets.image`Corona Borealis`,
     assets.image`Corvus`,
     assets.image`Crater`,
-    assets.image`Crux`
+    assets.image`Crux`,
+    assets.image`Cygnus`,
+    assets.image`Delphinus`,
+    assets.image`Dorado`,
+    assets.image`Draco`,
+    assets.image`Equuleus`
     ]
     Refs = [
     1,
@@ -535,7 +540,12 @@ function createImages () {
     27,
     28,
     29,
-    30
+    30,
+    31,
+    32,
+    33,
+    34,
+    35
     ]
 }
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
